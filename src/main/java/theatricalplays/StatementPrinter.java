a73e997360b2cf3c7bb5f6122ff5eaad36d0408b
TRABELSI Khalil <khaliltrabelsi2107@gmail.com>
2023-10-23 20:12:39 +0200

    Ajout du StringBuffer

---
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -0,0 +1,53 @@
+package theatricalplays;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+
+public class StatementPrinter {
+
+  public StringBuffer print(Invoice invoice, Map<String, Play> plays) {
+
+    int totalAmount = 0;
+    int volumeCredits = 0;
+    StringBuffer result = new StringBuffer(String.format("Statement for %s\n", invoice.customer));
+
+    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+
+    for (Performance perf : invoice.performances) {
+      Play play = plays.get(perf.playID);
+      int thisAmount = 0;
+
+      switch (play.type) {
+        case "tragedy":
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case "comedy":
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error("unknown type: ${play.type}");
+      }
+
+      // add volume credits
+      volumeCredits += Math.max(perf.audience - 30, 0);
+      // add extra credit for every ten comedy attendees
+      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+
+      // print line for this order
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
+      totalAmount += thisAmount;
+    }
+    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
+    result.append(String.format("You earned %s credits\n", volumeCredits));
+    return result;
+  }
+
+}
